27a4e26324f6f7ff945a7db8c71220177666d07e
kkumarasamy <kkumarasamy@edit-place.com>
2020-12-02 20:29:53 +0530

    bc-wine landing page | WIP

--- builds/development/landingPages/2020/December/01-12-2020/1/js/scripts.js
+++ builds/development/landingPages/2020/December/01-12-2020/1/js/scripts.js
@@ -62,7 +62,7 @@
             e.stopPropagation();
         });
         // make it as accordion for smaller screens
-        if ($(window).width() < 992) {
+/*         if ($(window).width() < 992) {
             $('.dropdown-menu a').click(function(e){
                 e.preventDefault();
                 if($(this).next('.submenu').length){
@@ -72,7 +72,27 @@
                 $(this).find('.submenu').hide();
                 })
             });
-        }
+        } */
         
     });
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (function() {
+        'use strict';
+        window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+            }, false);
+        });
+        }, false);
+    })();
+    
 })(jQuery); // End of use strict
