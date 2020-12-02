bdbccf14e08c1535f4ad552518ce148d4e6d8ad0
kkumarasamy <kkumarasamy@edit-place.com>
2020-12-02 12:28:10 +0530

    wine-bc landingpage | WIP

--- src/landingPages/2020/December/01-12-2020/1/js/scripts.js
+++ src/landingPages/2020/December/01-12-2020/1/js/scripts.js
@@ -53,4 +53,26 @@
     navbarCollapse();
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
+
+    
+    // jquery ready start
+    $(document).ready(function() {
+
+        $(document).on('click', '.dropdown-menu', function (e) {
+            e.stopPropagation();
+        });
+        // make it as accordion for smaller screens
+        if ($(window).width() < 992) {
+            $('.dropdown-menu a').click(function(e){
+                e.preventDefault();
+                if($(this).next('.submenu').length){
+                    $(this).next('.submenu').toggle();
+                }
+                $('.dropdown').on('hide.bs.dropdown', function () {
+                $(this).find('.submenu').hide();
+                })
+            });
+        }
+        
+    });
 })(jQuery); // End of use strict
